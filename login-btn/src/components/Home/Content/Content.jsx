9dd53834bb9293fa73c755357fef62a8e4d48cdc
adik-creator <abdul.usmanov@list.ru>
2022-04-29 16:38:31 +0600

    new

--- login-btn/src/components/Home/Content/Content.jsx
+++ login-btn/src/components/Home/Content/Content.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import c from "./Content.module.css";
+import scull from "./images/scull.png";
+import scull_livel_1 from "./images/scull_livel_1.png";
+import scull_livel_2 from "./images/scull_livel_2.png";
+import scull_eyes from "./images/scull_eyes.png";
 
 function Content() {
   return (
@@ -7,13 +11,13 @@ function Content() {
       <div className={c.wrapper}>
         <div className={c.inner}>
           <div className={c.image}>
-            <img src="./images/scull.png" alt="logo" className={c.image} />
+            <img src={scull} alt="logo" className={c.scull} />
           </div>
           <div className={c.text}>
-            <a href="#" className={c.a}>
+            <a href="#" className={c.content_a}>
               #1 Top Scariest Ghost 2020
             </a>
-            <h1 className={c.h1}>RESPAWN THE SPOOKY SKULL</h1>
+            <h1 className={c.content_h1}>RESPAWN THE SPOOKY SKULL</h1>
             <p className={c.content_p}>
               In search for cute little puppy, Captain Sem has come back from
               his tragic death. With his hogwarts certified power he promise to
