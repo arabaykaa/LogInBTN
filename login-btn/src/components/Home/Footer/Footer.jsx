3f6181ed2e86d4e91f1283f1a28b3dfb7a0771a3
arabaykaa <56497790+arabaykaa@users.noreply.github.com>
2022-04-30 15:20:49 +0600

    Last fixes from Arapbay

--- login-btn/src/components/Home/Footer/Footer.jsx
+++ login-btn/src/components/Home/Footer/Footer.jsx
@@ -3,18 +3,15 @@ import footerObj from "./Footer.module.css";
 
 export function Footer() {
   return (
-    <div>
-      <footer>
-        <div className={footerObj.footerContainer}>
-          <p>
-            <span>Captain Sem</span>
-            <br />
-            <br />
-            Veteran Spooky Ghost
-          </p>
-          <div></div>
-        </div>
-      </footer>
-    </div>
+    <footer>
+      <div className={footerObj.footerContainer}>
+        <p>
+          <span>Captain Sem</span>
+          <br />
+          Veteran Spooky Ghost
+        </p>
+        <div></div>
+      </div>
+    </footer>
   );
 }
--- login-btn/src/components/Home/Home.jsx
+++ login-btn/src/components/Home/Home.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Content from "./Content/Content";
 import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
+import bgColor from "./Home.module.css";
 
 function Home() {
   return (
-    <div>
+    <div className={bgColor.mainContainer}>
       <Header />
       <Content />
       <Footer />
