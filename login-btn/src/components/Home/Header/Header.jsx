6e73404cf59c104e6dcd3232e2dea49104d4aa1b
arabaykaa <56497790+arabaykaa@users.noreply.github.com>
2022-05-04 18:17:33 +0600

    Log-in/out btn is added

--- login-btn/src/components/Home/Header/Header.jsx
+++ login-btn/src/components/Home/Header/Header.jsx
@@ -1,29 +1,31 @@
 import React from "react";
-import headStyleObj from "./Header.module.css";
+import h from "./Header.module.css";
 
 export function Header() {
   return (
     <div>
       <header>
-        <div className={headStyleObj.headContainer}>
-          <div className={headStyleObj.headLogo}>&#128128;BRAINSTORM</div>
-          <div className={headStyleObj.headLinkContainer}>
+        <div className={h.headContainer}>
+          <div className={h.headLogo}>&#128128;BRAINSTORM</div>
+          <div className={h.headLinkContainer}>
             <ul>
               <li>
-                <a href="#">Graveyard</a>
+                <a href="">Graveyard</a>
               </li>
               <li>
-                <a href="#">Track Record</a>
+                <a href="">Track Record</a>
               </li>
               <li>
-                <a href="#">Services</a>
+                <a href="">Services</a>
               </li>
               <li>
-                <a href="#">Learn More</a>
+                <a href="">Learn More</a>
               </li>
             </ul>
-            <div className={headStyleObj.supportBtn}>
-              <a href="#">Support</a>
+            <div className={h.supportBtn}>
+              <a href="">LogIn</a>
+              <div className={h.vline}></div>
+              <a href="">LogOut</a>
             </div>
           </div>
         </div>
